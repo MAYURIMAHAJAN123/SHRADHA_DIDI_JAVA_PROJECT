5b2c3df276a1644c5860215db3504fe7b2ad91ac
mayuri <mayurimhjn123@gmail.com>
2023-10-05 23:09:57 +0530

    changes to sameline

--- src/package_1/class_2.java
+++ src/package_1/class_2.java
@@ -5,8 +5,8 @@ public class class_2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hi");
-
-		System.out.println("canceleded");
+		
+		System.out.println("canceleded now");
 
 	}
 
